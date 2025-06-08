5c281b3b8b2f1b9aff6a5dd17ff7fc2c8c74d8cb
Shinigami-kurosakiichigo <121304825+Shinigami-kurosakiichigo@users.noreply.github.com>
2025-06-09 01:48:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base:"/BlackAnt_DXF",
+  base:"/prj",
   plugins: [
     react(),
     mode === 'development' &&
